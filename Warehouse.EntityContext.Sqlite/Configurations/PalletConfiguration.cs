3c4e7cb720880c4f85bcd5d68c52cdb0022f0709
Debugg3R <debugg3r@gmail.com>
2022-07-23 23:50:20 +0300

    EFEntityConfigurations

---
+++ Warehouse.EntityContext.Sqlite/Configurations/PalletConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Warehouse.EntityContext.Models;
+
+namespace Warehouse.EntityContext.Sqlite.Configurations
+{
+    internal class PalletConfiguration : IEntityTypeConfiguration<PalletEntity>
+    {
+        public void Configure(EntityTypeBuilder<PalletEntity> builder)
+        {
+            builder.ToTable("Pallets")
+                .HasIndex("Id");
+
+            builder.HasKey(pallet => pallet.Id);
+
+            builder.Property(pallet => pallet.Length)
+                .HasColumnType("INTEGER")
+                .IsRequired();
+
+            builder.Property(pallet => pallet.Width)
+                .HasColumnType("INTEGER")
+                .IsRequired();
+
+            builder.Property(pallet => pallet.Height)
+                .HasColumnType("INTEGER")
+                .IsRequired();
+
+            builder.HasMany(pallet => pallet.Boxes);
+        }
+    }
+}
--- Warehouse.EntityContext.Sqlite/WarehouseSqliteContext.cs
+++ Warehouse.EntityContext.Sqlite/WarehouseSqliteContext.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
+using Warehouse.EntityContext.Sqlite.Configurations;
 using Warehouse.EntityContext.Models;
 
 namespace Warehouse.EntityContext.Sqlite;
@@ -46,80 +46,8 @@ public class WarehouseSqliteContext : DbContext, IWarehouseContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        DiscribePallet(modelBuilder);
-
-        DiscribeBox(modelBuilder);
+        modelBuilder.ApplyConfiguration(new PalletConfiguration());
+        modelBuilder.ApplyConfiguration(new BoxConfiguration());
     }
 
-    private void DiscribeBox(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<BoxEntity>()
-            .ToTable("Boxes")
-            .HasIndex("Id");
-
-        modelBuilder.Entity<BoxEntity>()
-            .HasKey(box => box.Id);
-
-        modelBuilder.Entity<BoxEntity>()
-            .Property(box => box.Length)
-            .HasColumnType("INTEGER")
-            .IsRequired();
-
-        modelBuilder.Entity<BoxEntity>()
-            .Property(box => box.Width)
-            .HasColumnType("INTEGER")
-            .IsRequired();
-
-        modelBuilder.Entity<BoxEntity>()
-            .Property(box => box.Height)
-            .HasColumnType("INTEGER")
-            .IsRequired();
-
-        modelBuilder.Entity<BoxEntity>()
-            .Property(box => box.Weight)
-            .HasColumnType("INTEGER")
-            .IsRequired();
-
-        modelBuilder.Entity<BoxEntity>()
-            .Property(box => box.ExpirationDate)
-            .HasColumnType("datetime")
-            .IsRequired();
-
-        modelBuilder.Entity<BoxEntity>()
-            .Property(box => box.PalletModelId)
-            .HasColumnName("PalletId")
-            .HasColumnType("INTEGER");
-
-        modelBuilder.Entity<BoxEntity>()
-            .HasOne(box => box.PalletModel)
-            .WithMany(pallet => pallet.Boxes);
-    }
-
-    private void DiscribePallet(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<PalletEntity>()
-            .ToTable("Pallets")
-            .HasIndex("Id");
-
-        modelBuilder.Entity<PalletEntity>()
-            .HasKey(pallet => pallet.Id);
-
-        modelBuilder.Entity<PalletEntity>()
-            .Property(pallet => pallet.Length)
-            .HasColumnType("INTEGER")
-            .IsRequired();
-
-        modelBuilder.Entity<PalletEntity>()
-            .Property(pallet => pallet.Width)
-            .HasColumnType("INTEGER")
-            .IsRequired();
-
-        modelBuilder.Entity<PalletEntity>()
-            .Property(pallet => pallet.Height)
-            .HasColumnType("INTEGER")
-            .IsRequired();
-
-        modelBuilder.Entity<PalletEntity>()
-            .HasMany(pallet => pallet.Boxes);
-    }
 }
